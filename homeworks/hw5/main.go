54cb014c78f40c927f29b10124de065f4009ef79
daniyarmax <aktau570404@gmail.com>
2026-07-04 00:15:37 +0500

    Console calculator for logistics

---
+++ homeworks/hw5/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const (
+		baseRate     = 5.50
+		taxRate      = 0.12
+		distanceRate = 2.0
+		fragileFee   = 0.2
+	)
+
+	var name string
+	var distance float64
+	var weight float64
+	var numberOfFragileItems int
+
+	fmt.Print("Enter your name: ")
+	fmt.Scanln(&name)
+
+	fmt.Print("Enter the distance (in km): ")
+	fmt.Scanln(&distance)
+
+	fmt.Print("Enter the weight of the package (in kg): ")
+	fmt.Scanln(&weight)
+
+	fmt.Print("Enter the number of fragile items: ")
+	fmt.Scanln(&numberOfFragileItems)
+
+	baseCost := (weight * baseRate) + (distance * distanceRate) + (1 + float64(numberOfFragileItems)*fragileFee)
+
+	finalCost := baseCost * (1 + taxRate)
+	fmt.Println("--- Delivery information ---")
+	fmt.Printf("Name: %s\n", name)
+	fmt.Printf("Final cost: %.2f\n", finalCost)
+}
